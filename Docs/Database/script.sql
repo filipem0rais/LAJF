cb2666aeab39a67fde66c56899dc7eb66055fd91
fildiasmora <filipe.diasmorais@gmail.com>
2023-04-25 16:35:07 +0200

    Update Script
    Ajout de la colonne "nom" dans "item"

--- Docs/Database/script.sql
+++ Docs/Database/script.sql
@@ -1,137 +1,97 @@
--- *********************************************
--- * Standard SQL generation                   
--- *--------------------------------------------
--- * DB-MAIN version: 11.0.2              
--- * Generator date: Sep 14 2021              
--- * Generation date: Tue Apr 25 10:33:48 2023 
--- * LUN file: C:\DATA\LAJF\Docs\Database\LAJF.lun 
--- * Schema: LAJF/SQL 
--- ********************************************* 
-
-
--- Database Section
--- ________________ 
-
-drop database if exists LAJF;
-create database LAJF;
-use LAJF;
-
--- DBSpace Section
--- _______________
-
-
--- Tables Section
--- _____________ 
-
-create table Categories (
-     idCategory int not null auto_increment,
-     catName varchar(100) not null,
-     catParent int,
-     constraint ID_Categories_ID primary key (idCategory));
-
-create table Items (
-     idItem int not null auto_increment,
-     iteDescription varchar(255) not null,
-     iteInitialValue decimal(15,2) not null,
-     iteOnSale bool not null default 1,
-     iteDatePublication datetime not null default current_timestamp,
-     idUser int not null,
-     idCategory int not null,
-     constraint ID_Items_ID primary key (idItem));
-
-create table Bid (
-     idBid int not null auto_increment,
-     bidAmount decimal(15,2) not null,
-     idUser int not null,
-     idItem int not null,
-     constraint ID_Bid_ID primary key (idBid));
-
-create table Sale (
-     idSale int not null auto_increment,
-     idItem int not null,
-     salPickedUp bool not null default 0,
-     idUser int not null,
-     constraint ID_Sale_ID primary key (idSale),
-     constraint SID_Sale_Items_ID unique (idItem));
-
-create table Users (
-     idUser int not null auto_increment,
-     useName varchar(100) not null,
-     useLastName varchar(100) not null,
-     useEmail varchar(255) not null,
-     usePassword varchar(255) not null,
-     useCredit decimal(15,2) not null default 0.00,
-     constraint ID_Users_ID primary key (idUser));
-
-
--- Constraints Section
--- ___________________ 
-
-alter table Categories add constraint REF_Categ_Categ_FK
-     foreign key (catParent)
-     references Categories (idCategory);
-
-alter table Items add constraint REF_Items_Users_FK
-     foreign key (idUser)
-     references Users (idUser);
-
-alter table Items add constraint REF_Items_Categ_FK
-     foreign key (idCategory)
-     references Categories (idCategory);
-
-alter table Bid add constraint REF_Bid_Users_FK
-     foreign key (idUser)
-     references Users (idUser);
-
-alter table Bid add constraint REF_Bid_Items_FK
-     foreign key (idItem)
-     references Items (idItem);
-
-alter table Sale add constraint REF_Sale_Users_FK
-     foreign key (idUser)
-     references Users (idUser);
-
-alter table Sale add constraint SID_Sale_Items_FK
-     foreign key (idItem)
-     references Items (idItem);
-
-
--- Index Section
--- _____________ 
-
-create unique index ID_Categories_IND
-     on Categories (idCategory);
+create table categories
+(
+    idCategory int auto_increment
+        primary key,
+    catName    varchar(100) not null,
+    catParent  int          null,
+    constraint ID_Categories_IND
+        unique (idCategory),
+    constraint REF_Categ_Categ_FK
+        foreign key (catParent) references categories (idCategory)
+);
 
 create index REF_Categ_Categ_IND
-     on Categories (catParent);
+    on categories (catParent);
 
-create unique index ID_Items_IND
-     on Items (idItem);
+create table users
+(
+    idUser      int auto_increment
+        primary key,
+    useName     varchar(100)                not null,
+    useLastName varchar(100)                not null,
+    useEmail    varchar(255)                not null,
+    usePassword varchar(255)                not null,
+    useCredit   decimal(15, 2) default 0.00 not null,
+    constraint ID_Users_IND
+        unique (idUser)
+);
 
-create index REF_Items_Users_IND
-     on Items (idUser);
+create table items
+(
+    idItem             int auto_increment
+        primary key,
+    iteDescription     varchar(255)                         not null,
+    iteInitialValue    decimal(15, 2)                       not null,
+    iteOnSale          tinyint(1) default 1                 not null,
+    iteDatePublication datetime   default CURRENT_TIMESTAMP not null,
+    idUser             int                                  not null,
+    idCategory         int                                  not null,
+    iteNom             varchar(100)                         not null,
+    constraint ID_Items_IND
+        unique (idItem),
+    constraint REF_Items_Categ_FK
+        foreign key (idCategory) references categories (idCategory),
+    constraint REF_Items_Users_FK
+        foreign key (idUser) references users (idUser)
+);
 
-create index REF_Items_Categ_IND
-     on Items (idCategory);
-
-create unique index ID_Bid_IND
-     on Bid (idBid);
-
-create index REF_Bid_Users_IND
-     on Bid (idUser);
+create table bid
+(
+    idBid     int auto_increment
+        primary key,
+    bidAmount decimal(15, 2) not null,
+    idUser    int            not null,
+    idItem    int            not null,
+    constraint ID_Bid_IND
+        unique (idBid),
+    constraint REF_Bid_Items_FK
+        foreign key (idItem) references items (idItem),
+    constraint REF_Bid_Users_FK
+        foreign key (idUser) references users (idUser)
+);
 
 create index REF_Bid_Items_IND
-     on Bid (idItem);
+    on bid (idItem);
 
-create unique index ID_Sale_IND
-     on Sale (idSale);
+create index REF_Bid_Users_IND
+    on bid (idUser);
+
+create index REF_Items_Categ_IND
+    on items (idCategory);
+
+create index REF_Items_Users_IND
+    on items (idUser);
+
+create table sale
+(
+    idSale      int auto_increment
+        primary key,
+    idItem      int                  not null,
+    salPickedUp tinyint(1) default 0 not null,
+    idUser      int                  not null,
+    constraint ID_Sale_IND
+        unique (idSale),
+    constraint SID_Sale_Items_ID
+        unique (idItem),
+    constraint SID_Sale_Items_IND
+        unique (idItem),
+    constraint REF_Sale_Users_FK
+        foreign key (idUser) references users (idUser),
+    constraint SID_Sale_Items_FK
+        foreign key (idItem) references items (idItem)
+);
 
 create index REF_Sale_Users_IND
-     on Sale (idUser);
+    on sale (idUser);
 
-create unique index SID_Sale_Items_IND
-     on Sale (idItem);
-
-create unique index ID_Users_IND
-     on Users (idUser);
 
