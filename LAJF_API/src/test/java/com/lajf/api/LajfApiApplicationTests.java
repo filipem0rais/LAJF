f50b1e509e187f692e8ad4de9b623713f26fbead
fildiasmora <filipe.diasmorais@gmail.com>
2023-05-01 10:22:11 +0200

    HelloWorld SpringBoot

---
+++ LAJF_API/src/test/java/com/lajf/api/LajfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lajf.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LajfApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
