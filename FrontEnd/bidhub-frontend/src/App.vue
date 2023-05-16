2e391e3c0aae008bd1ed6f386500b1e6fa9a4598
lucbohlen <lucabohlen@hotmail.fr>
2023-05-16 09:11:09 +0200

    FrontEnd
    BreadCrum + navbar + identification + footer

--- FrontEnd/bidhub-frontend/src/App.vue
+++ FrontEnd/bidhub-frontend/src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <menu-bar/>
   <router-view/>
+  <footer-bar/>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
+  margin-right: auto;
+  margin-left: auto;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+<script setup>
+import MenuBar from '@/components/MenuBar.vue'
+import FooterBar from '@/components/FooterBar.vue'
+</script>
---
+++ FrontEnd/bidhub-frontend/src/views/IdentificationView.vue
@@ -0,0 +1,81 @@
+<template>
+  <body>
+  <bread-crum/>
+  <div class="main-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <h2 class="card-title">Se connecter</h2>
+              <form>
+                <div class="mb-3">
+                  <label for="email" class="form-label">Adresse e-mail</label>
+                  <input type="email" class="form-control" id="email" required>
+                </div>
+                <div class="mb-3">
+                  <label for="password" class="form-label">Mot de passe</label>
+                  <input type="password" class="form-control" id="password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Se connecter</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <h2 class="card-title">S'inscrire</h2>
+              <form>
+                <div class="mb-3">
+                  <label for="firstname" class="form-label">Prénom</label>
+                  <input type="text" class="form-control" id="firstname" required>
+                </div>
+                <div class="mb-3">
+                  <label for="lastname" class="form-label">Nom</label>
+                  <input type="text" class="form-control" id="lastname" required>
+                </div>
+                <div class="mb-3">
+                  <label for="email2" class="form-label">Adresse e-mail</label>
+                  <input type="email" class="form-control" id="email2" required>
+                </div>
+                <div class="mb-3">
+                  <label for="password2" class="form-label">Mot de passe</label>
+                  <input type="password" class="form-control" id="password2" required>
+                </div>
+                <div class="mb-3">
+                  <label for="confirm-password" class="form-label">Confirmez le mot de passe</label>
+                  <input type="password" class="form-control" id="confirm-password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">S'inscrire</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
+</template>
+
+<style scoped>
+.btn {
+  background-color: #ED7D2F;
+  border-color: #ED7D2F;
+}
+
+.card-title {
+  padding-bottom: 10px;
+}
+</style>
+
+<script>
+import BreadCrum from '@/components/BreadCrum.vue'
+
+export default {
+  name: 'IdentificationView',
+  components: {
+    BreadCrum
+  }
+}
+</script>
